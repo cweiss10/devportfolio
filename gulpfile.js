ae04f55bdc066a0e00b474b7b4643d72ff760f7a
Cat Weiss <catherineweiss95@gmail.com>
2024-09-09 22:12:06 -0700

    update packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 var plumber = require('gulp-plumber');
 var uglify = require('gulp-uglify');
-var sass = require('gulp-sass');
 var wait = require('gulp-wait');
 var rename = require('gulp-rename');
 var autoprefixer = require('gulp-autoprefixer');
@@ -26,7 +25,6 @@ gulp.task('scripts', function() {
 gulp.task('styles', function () {
     return gulp.src('./scss/styles.scss')
         .pipe(wait(250))
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(gulp.dest('./css'));
 });
 
